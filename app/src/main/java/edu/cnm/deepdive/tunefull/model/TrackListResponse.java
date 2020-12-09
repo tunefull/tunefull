6fd92f39b2fcee4711b4458834a2b17ac057092b
Robert Dominguez <dominguez.1st@gmail.com>
2020-12-09 16:18:00 -0700

    Added documentation to User.java

--- app/src/main/java/edu/cnm/deepdive/tunefull/model/TrackListResponse.java
+++ app/src/main/java/edu/cnm/deepdive/tunefull/model/TrackListResponse.java
@@ -4,15 +4,29 @@ import com.google.gson.annotations.Expose;
 import com.spotify.protocol.types.Track;
 import java.util.List;
 
+/**
+ * Holds data in the database for the track list responses.
+ */
 public class TrackListResponse {
+
 
   @Expose
   private List<ResponseItem> items;
 
+  /**
+   * Returns a list of items.
+   *
+   * @return
+   */
   public List<ResponseItem> getItems() {
     return items;
   }
 
+  /**
+   * Sets a list of items.
+   *
+   * @param items The list of items.
+   */
   public void setItems(List<ResponseItem> items) {
     this.items = items;
   }
@@ -22,10 +36,20 @@ public class TrackListResponse {
     @Expose
     private Track track;
 
+    /**
+     * Returns a song track.
+     *
+     * @return
+     */
     public Track getTrack() {
       return track;
     }
 
+    /**
+     * Sets a song track.
+     *
+     * @param track The song track.
+     */
     public void setTrack(Track track) {
       this.track = track;
     }
